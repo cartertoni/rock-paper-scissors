cc319666945061652fc98a3a023978661a1890c2
Carter Toni <carter.blaine.toni@gmail.com>
2023-08-29 11:35:31 -0700

    Add user interface to play first-to-5-wins match

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let playerWins = 0
 let computerWins = 0
-let ties = 0
+let tiedGames = 0
 
 const getComputerChoice = () => {
   let random = Math.floor(Math.random() * 3)
@@ -9,15 +9,15 @@ const getComputerChoice = () => {
   else return 'scissors'
 }
 
-const getPlayerChoice = () => {
-  let choice
-  do
-    choice = prompt(
-      'What is your choice: rock, paper, or scissors?'
-    ).toLowerCase()
-  while (choice !== 'rock' && choice !== 'paper' && choice !== 'scissors')
-  return choice
-}
+// const getPlayerChoice = () => {
+//   let choice
+//   do
+//     choice = prompt(
+//       'What is your choice: rock, paper, or scissors?'
+//     ).toLowerCase()
+//   while (choice !== 'rock' && choice !== 'paper' && choice !== 'scissors')
+//   return choice
+// }
 
 const determineWinner = (computerChoice, playerChoice) => {
   let winner
@@ -39,23 +39,50 @@ const determineWinner = (computerChoice, playerChoice) => {
 
   if (winner === 'player') {
     playerWins += 1
-    return `You win! ${playerChoice} beats ${computerChoice}`
+    checkForMatchWinner(`You win! ${playerChoice} beats ${computerChoice}`)
   } else if (winner === 'computer') {
     computerWins += 1
-    return `You lose! ${computerChoice} beats ${playerChoice}`
+    checkForMatchWinner(`You lose! ${computerChoice} beats ${playerChoice}`)
   } else {
-    ties += 1
-    return `Tie! ${computerChoice} ties ${playerChoice}`
+    tiedGames += 1
+    checkForMatchWinner(`Tie! ${computerChoice} ties ${playerChoice}`)
   }
+  return
 }
 
-const game = () => {
-  for (i = 0; i < 5; i++) {
-    console.log(determineWinner(getComputerChoice(), getPlayerChoice()))
-  }
-  console.log(
-    `GAME OVER! \nPlayer wins: ${playerWins} \nComputer wins: ${computerWins} \nTies: ${ties}`
+const buttons = document.querySelectorAll('button')
+
+buttons.forEach(button =>
+  button.addEventListener('click', () =>
+    determineWinner(getComputerChoice(), button.classList.value)
   )
+)
+
+const checkForMatchWinner = message => {
+  if (playerWins === 5) {
+    updateDOM(`Player has won the match!`)
+    endMatch()
+  } else if (computerWins === 5) {
+    updateDom(`Computer has won the match!`)
+    endMatch()
+  } else updateDOM(message)
 }
 
-game()
+const updateDOM = message => {
+  const outcome = document.querySelector('.outcome')
+  outcome.textContent = message
+
+  const player = document.querySelector('.player-wins')
+  player.textContent = `Player wins: ${playerWins}`
+
+  const computer = document.querySelector('.computer-wins')
+  computer.textContent = `Computer wins: ${computerWins}`
+
+  const ties = document.querySelector('.ties')
+  ties.textContent = `Ties: ${tiedGames}`
+}
+
+const endMatch = () => {
+  const buttons = document.querySelectorAll('button')
+  buttons.forEach(button => (button.disabled = true))
+}
